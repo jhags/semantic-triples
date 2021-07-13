27354e273e40b917f8023f263a4371387c7573dd
Johan Hagstrom <johan.hagstrom@mottmac.com>
2021-07-13 17:31:04 +0100

    triples improved

--- scripts/dev.py
+++ scripts/dev.py
@@ -6,25 +6,53 @@ import spacy
 from spacy import displacy
 import networkx as nx
 import matplotlib.pyplot as plt
+import string
+
+import wikipedia
 
 root = os.path.abspath(os.path.join(__file__, "../.."))
 sys.path.append(root + r'/scripts')
 
 # pylint: disable=import-error
-from semantic_triples import get_triples
+from semantic_triples import get_triples, Node
 
 # Get Spacy language packages
 nlp = spacy.load('en_core_web_sm')
 
+PRONS = set(open('spacy_prons.txt', 'r').read().split())
 
-sentence = "London is the capital and largest city of England and the United Kingdom. Standing on the River Thames in the south-east of England, at the head of its 50-mile (80 km) estuary leading to the North Sea, London has been a major settlement for two millennia. Londinium was founded by the Romans. The City of London, London’s ancient core − an area of just 1.12 square miles (2.9 km2) and colloquially known as the Square Mile − retains boundaries that follow closely its medieval limits. The City of Westminster is also an Inner London borough holding city status. Greater London is governed by the Mayor of London and the London Assembly. London is located in the southeast of England.Westminster is located in London. London is the biggest city in Britain. London has a population of 7,172,036."
+def remove_punctuation(text_str):
+    chars = '()−' #string.punctuation
+    for char in chars:
+        text_str = text_str.replace(char, ' ')
 
-sentence = "The City of London, London’s ancient core an area of just 1.12 square miles (2.9 km2) and colloquially known as the Square Mile retains boundaries that follow closely its medieval limits."
+    chars = '-' #string.punctuation
+    for char in chars:
+        text_str = text_str.replace(char, '')
 
-sentence = 'Mike said triples can be objects'
+    return ' '.join(text_str.split())
+
+# page = wikipedia.page('COVID-19 pandemic')
+# page.title
+# sentence = page.summary
+
+# sentence = "London is the capital and largest city of England and the United Kingdom. Standing on the River Thames in the south-east of England, at the head of its 50-mile (80 km) estuary leading to the North Sea, London has been a major settlement for two millennia. Londinium was founded by the Romans. The City of London, London’s ancient core an area of just 1.12 square miles (2.9 km2) and colloquially known as the Square Mile retains boundaries that follow closely its medieval limits. The City of Westminster is also an Inner London borough holding city status. Greater London is governed by the Mayor of London and the London Assembly. London is located in the southeast of England.Westminster is located in London. London is the biggest city in Britain. London has a population of 7,172,036."
+
+sentence = "The ongoing global pandemic of coronavirus disease 2019 (COVID-19) is caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The virus was first identified in December 2019 in Wuhan, China. The World Health Organization declared a Public Health Emergency of International Concern on 30 January 2020, and later declared a pandemic on 11 March 2020. As of 9 July 2021, more than 185 million cases have been confirmed, with more than 4.01 million confirmed deaths attributed to COVID-19, making it one of the deadliest pandemics in history.The severity of COVID-19 symptoms is highly variable, ranging from unnoticeable to life-threatening. Severe illness is more likely in elderly COVID-19 patients, as well as those who have certain underlying medical conditions. COVID-19 transmits when people breathe in air contaminated by droplets and small airborne particles. The risk of breathing these in is highest when people are in close proximity, but they can be inhaled over longer distances, particularly indoors. Transmission can also occur if splashed or sprayed with contaminated fluids, in the eyes, nose or mouth, and, rarely, via contaminated surfaces. People remain contagious for up to 20 days, and can spread the virus even if they do not develop any symptoms.Recommended preventive measures include social distancing, wearing face masks in public, ventilation and air-filtering, hand washing, covering one's mouth when sneezing or coughing, disinfecting surfaces, and monitoring and self-isolation for people exposed or symptomatic. Several vaccines have been developed and widely distributed in most developed countries since December 2020. Current treatments focus on addressing symptoms, but work is underway to develop medications that inhibit the virus. Authorities worldwide have responded by implementing travel restrictions, lockdowns and quarantines, workplace hazard controls, and business closures. Numerous jurisdictions have also worked to increase testing capacity and trace contacts of the infected. The pandemic has resulted in severe global, social and economic disruption, including the largest global recession since the Great Depression of the 1930s. It has led to widespread supply shortages exacerbated by panic buying, agricultural disruption, and food shortages. However, it has also caused temporary decreases in emissions of pollutants and greenhouse gases. Numerous educational institutions and public areas have been partially or fully closed, and many events have been cancelled or postponed. Misinformation has circulated through social media and mass media, and political tensions have been exacerbated. The pandemic has raised issues of racial and geographic discrimination, health equity, wealth inequality and the balance between public health imperatives and individual rights."
+# sentence = "The City of London, London’s ancient core an area of just 1.12 square miles (2.9 km2) and colloquially known as the Square Mile retains boundaries that follow closely its medieval limits."
+
+
+# sentence = "The World Health Organization declared a Public Health Emergency of International Concern on 30 January 2020, and later declared a pandemic on 11 March 2020."
+# sentence = "Recommended preventive measures include social distancing, wearing face masks in public, ventilation and air-filtering, hand washing, covering one's mouth when sneezing or coughing, disinfecting surfaces, and monitoring and self-isolation for people exposed or symptomatic."
+# sentence = "The ongoing global pandemic of coronavirus disease 2019 (COVID-19) is caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2)."
+
+sentence = ' '.join([i for i in sentence.split() if i not in PRONS])
+sentence = remove_punctuation(sentence)
+
 doc = nlp(sentence)
 
-get_triples(doc)
+# displacy.render(doc)
+triples = get_triples(doc)
 
 edges = []
 for trp in triples:
@@ -34,6 +62,10 @@ edges = list(set(edges))
 g = nx.DiGraph()
 g.add_edges_from(edges)
 g.remove_nodes_from(list(nx.isolates(g)))
-pos = nx.spring_layout(g, seed=227)
-nx.draw(g, pos, with_labels=True, node_size=500)
-plt.margins(x=0.5)
+# pos = nx.spring_layout(g, seed=227)
+# nx.draw(g, pos, with_labels=True, node_size=500)
+# plt.margins(x=0.5)
+
+nx.write_gexf(g, 'triple.gexf')
+
+
--- scripts/semantic_triples.py
+++ scripts/semantic_triples.py
@@ -3,7 +3,9 @@ import os
 import pandas as pd
 import spacy
 import string
+from typing import TypeVar
 
+spacyToken = TypeVar('spacy.tokens.token.Token')
 
 root = os.path.abspath(os.path.join(__file__, "../.."))
 
@@ -15,16 +17,15 @@ class Node:
     def __init__(self, spacy_obj):
         self.span = spacy_obj
 
-        if isinstance(spacy_obj, spacy.tokens.span.Span):
-            self.root = spacy_obj.root.text
-            self.phrase = ' '.join([t.text for t in spacy_obj if t.pos_ not in ['DET']])
-            self.text = ' '.join([t.lemma_ for t in spacy_obj if t.pos_ not in ['DET']])
-            self.idx = (spacy_obj.start_char, spacy_obj.end_char)
+        # if isinstance(spacy_obj, spacy.tokens.span.Span):
+        #     self.root = spacy_obj.root.lemma_
+        #     self.text = ' '.join([t.lemma_.lower() for t in spacy_obj if t.pos_ not in ['DET']])
+        #     self.idx = (spacy_obj.start_char, spacy_obj.end_char)
 
-        elif isinstance(spacy_obj, spacy.tokens.token.Token):
-            self.root = spacy_obj.text
-            self.phrase = spacy_obj.text
-            self.text = spacy_obj.text
+        if isinstance(spacy_obj, spacy.tokens.token.Token):
+            self.phrase = self._get_full_phrase(spacy_obj)
+            self.root = spacy_obj.text.lower()
+            self.text = spacy_obj.lemma_.lower()
             self.idx = (spacy_obj.idx, spacy_obj.idx + len(spacy_obj.text))
 
         elif spacy_obj is None:
@@ -36,108 +37,123 @@ class Node:
     def __repr__(self):
         return self.text
 
-
-class Triple:
-    def __init__(self, subj, predicate, obj):
-        self.subj = self._get_full_phrase(subj)
-        self.predicate = self._get_full_phrase(predicate)
-        self.obj = self._get_full_phrase(obj)
-        self.spo = (self.subj.text, self.predicate.text, self.obj.text)
-
-    def __repr__(self):
-        return f'({self.subj} -> {self.predicate} -> {self.obj})'
-
     @staticmethod
     def _get_full_phrase(spacy_token):
-        if spacy_token is None:
-            return Node(spacy_token)
         doc = spacy_token.doc
         start_idx = spacy_token.idx
         chunk = [i for i in doc.noun_chunks if (i.start_char <= start_idx <= i.end_char)]
         if len(chunk)>0:
-            return Node(chunk[0])
-        return Node(spacy_token)
+            return ' '.join([t.lemma_.lower() for t in chunk[0] if t.pos_ not in ['DET']])
+            # return Node(chunk[0])
+        return spacy_token.lemma_.lower()
+
+
+class Triple:
+    def __init__(self, subj, predicate, obj):
+        self.subj = Node(subj)
+        self.predicate = Node(predicate)
+        self.obj = Node(obj)
+        self.spo = (self.subj.text, self.predicate.text, self.obj.text)
+
+    def __repr__(self):
+        return f'({self.subj.phrase} -> {self.predicate.text} -> {self.obj.phrase})'
 
     def edges(self):
         if self.predicate.span is None:
-            return [(self.subj.text, self.obj.text)]
+            return [(self.subj.phrase, self.obj.phrase)]
         else:
             return [
-                (self.subj.text, self.predicate.text),
-                (self.predicate.text, self.obj.text)
+                (self.subj.phrase, self.predicate.text),
+                (self.predicate.text, self.obj.phrase)
                 ]
 
 
-def remove_punctuation(text_str):
-    chars = string.punctuation + '−'
-    for char in chars:
-        text_str = text_str.replace(char, ' ')
-    return ' '.join(text_str.split())
+def get_primary_predicate(subject: spacyToken):
+    primary_predicate = subject.head
+
+    if primary_predicate.pos_ in ['VERB', 'ADP', 'AUX']:
+        if primary_predicate.pos_ in ['ADP']:
+            if primary_predicate.head.pos_ in ['VERB']:
+                primary_predicate = primary_predicate.head
+    return primary_predicate
 
 
-def find_child_objects(spacy_token):
-    children = list(spacy_token.children)
+def get_child_dep_dict(token: spacyToken, d=None):
+    children = list(token.children)
     children_dep = [child.dep_ for child in children]
-    children_dict = dict(zip(children_dep, children))
-
-    primary_deps = ['pobj', 'dobj', 'attr', 'ccomp']
-    secondary_deps = ['prep', 'agent', 'relcl', 'acl']
-    for dep in (primary_deps + secondary_deps):
-        token = children_dict.get(dep)
-        if token is not None:
-            if dep in secondary_deps:
-                return find_child_objects(token)
-            return token
-
-
-def find_secondary_predicate(spacy_token):
-    for child in list(spacy_token.children):
-        if child.dep_ in ['prep', 'agent', 'relcl', 'acl']: return child
+    if d is None:
+        d = {}
+    for dep, child in zip(children_dep, children):
+        v = d.get(dep)
+        if v is None:
+            d[dep] = [child]
+        else:
+            v.append(child)
+    return d
 
 
-def get_subjects(node_dict):
-    subjects = []
-    for v in node_dict.values():
-        if v['type']=='subject':
-            chunk = v['object']
-            subjects.append((chunk.root, chunk))
-    return subjects
+def get_secondary_predicates(primary_predicate: spacyToken):
+    dep_dict = get_child_dep_dict(primary_predicate)
+    deps = ['conj', 'advcl']
+    tokens = []
+    for dep in deps:
+        token = dep_dict.get(dep, [])
+        tokens.extend(token)
+    return tokens
+
+
+def get_objects(predicate: spacyToken):
+    d = get_child_dep_dict(predicate)
+    deps = ['pobj', 'dobj', 'attr', 'ccomp', 'appos']
+    lvl2_dep = ['prep', 'agent', 'relcl', 'acl']
+    tokens = []
+    for dep, toks in d.items():
+        if dep in deps:
+            tokens.extend(toks)
+
+        elif dep in lvl2_dep:
+            for tok in toks:
+                t = get_objects(tok)
+                tokens.extend(t)
+    return tokens
 
 
 def get_triples(doc):
     subjects = [i.root for i in doc.noun_chunks if i.root.dep_ in ['nsubj', 'nsubjpass']]
-
     triples = []
     for subject in subjects:
-        primary_predicate, primary_obj = None, None
-        primary_predicate = subject.head
 
-        if primary_predicate.pos_ in ['VERB', 'ADP', 'AUX']:
-            if primary_predicate.pos_ in ['ADP']:
-                if primary_predicate.head.pos_ in ['VERB']:
-                    primary_predicate = primary_predicate.head
+        primary_subject = subject
+        primary_predicate = get_primary_predicate(primary_subject)
+        primary_objects = get_objects(primary_predicate)
 
-        primary_obj = find_child_objects(primary_predicate)
-        triples.append(Triple(subject, primary_predicate, primary_obj))
+        for obj in primary_objects:
+            triples.append(Triple(primary_subject, primary_predicate, obj))
 
-        if primary_obj is not None:
-            level2_obj = find_child_objects(primary_obj)
-            level2_predicate = find_secondary_predicate(primary_obj)
+        # Additional primary objects
+        primary_objects = get_objects(primary_subject)
+        for obj in primary_objects:
+            triples.append(Triple(primary_subject, None, obj))
 
-        if (level2_obj is not None) & (level2_predicate is not None):
-            triples.append(Triple(subject, level2_predicate, level2_obj))
+        # Secondary triples, where the primary subject is the same
+        secondary_predicates = get_secondary_predicates(primary_predicate)
+        for predicate in secondary_predicates:
+            objs = get_objects(predicate)
+            for obj in objs:
+                triples.append(Triple(primary_subject, predicate, obj))
 
-        secondary_predicates = []
-        for child in list(subject.children):
-            if child.dep_ in ['prep', 'agent', 'relcl', 'acl', 'conj', 'appos']: 
-                secondary_predicates.append(child)
+        # 2nd level objects
+        objs = [x.obj.span for x in triples]
+        for obj in objs:
+            lvl2_objs = get_objects(obj)
+            for obj2 in lvl2_objs:
+                triples.append(Triple(obj, None, obj2))
 
-        sec_objs = []
-        for p in secondary_predicates:
-            sec_objs.append(find_child_objects(p))
+        # 3rd level objects
+        objs = [x.obj.span for x in triples]
+        for obj in objs:
+            lvl3_objs = get_objects(obj)
+            for obj3 in lvl3_objs:
+                triples.append(Triple(obj, None, obj3))
 
-        for (p, o) in zip(secondary_predicates, sec_objs):
-            if (p is not None) & (o is None):
-                p, o = o, p
-            triples.append(Triple(subject, p, o))
     return triples
